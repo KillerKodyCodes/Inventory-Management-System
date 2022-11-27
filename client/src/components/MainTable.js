70c3c41dfde5f02b50a68460434b6738a659a27c
KillerKodyCodes <killerbyrd12@gmail.com>
2022-11-27 14:09:29 -0600

    Missed things in sync. Corrected.

---
+++ client/src/components/MainTable.js
@@ -0,0 +1,65 @@
+/*
+    Add param to update to link to specific category to update
+    Format "Updated" date
+    Change ID to better format (Ex. 10000, 20000)
+*/
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+
+export default function MainTable() {
+
+  const [listOfInventory, setListOfInventory] = useState([])
+
+  useEffect(() => {
+  axios.get("http://localhost:3001/inventory").then((response) => {
+      setListOfInventory(response.data)
+  })
+  }, [])
+
+  const navigate = useNavigate()
+    
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Category</TableCell>
+            <TableCell align="right">In Stock</TableCell>
+            <TableCell align="right">Updated</TableCell>
+            <TableCell align="right">Edit</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {listOfInventory.map((row) => (
+                <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <TableCell component="th" scope="row">{row.id}</TableCell>
+                    <TableCell align="right">{row.cat_name}</TableCell>
+                    <TableCell align="right">{row.count}</TableCell>
+                    <TableCell align="right">{row.updatedAt}</TableCell>
+                    <TableCell align="right">
+                        <IconButton aria-label="edit" onClick={() => {navigate(`/Update/${row.id}`)}}>
+                            <EditIcon />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,18 +5,9 @@ function Home() {
 
   return (
     <div className="App">
-      {
-        listOfInventory.map((value, key) => {
-          return(
-            <div className="container">
-              <div className="title">perishable: { value.perishable }</div>
-              <div className="body">frozen: { value.frozen }</div>
-              <div className="footer">produce: { value.produce }</div>
-              <div className="footer">dried: { value.dried }</div>
-            </div>
-          )
-        })
-      }
+      <div className="container">
+        <MainTable />
+      </div>
     </div>
   );
 }
